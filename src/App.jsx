6139cc25f8d4a8041881a13619e8ac3cbeb20d68
Anjum Ali <engr.anjumali@gmail.com>
2025-01-11 20:21:33 +0500

    Initialize project with vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center min-h-screen items-center'>Hello World</div>
+    </>
+  )
+}
+
+export default App
